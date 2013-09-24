cee2a6dc3a0a7a7bb29efbbdd34c55873e3edeb2
viktorberzsinszky <vberzsin@gmail.com>
2013-09-24 16:34:31 +0200

    Download and index life documents to local Couchbase and Elasticserach instances

--- ithis.js
+++ ithis.js
@@ -29,36 +29,39 @@ require("request")("http://127.0.0.1:8092/default/_design/export/_view/all?conne
 		if(response){
 			console.log("response: " +response);
 		} 
-		if(body){
-			client = new Elastical.Client("127.0.0.1", {port: 9200});
-			res = JSON.parse(body);
-			console.log(res);
-			for(index in res.rows){
-				doc = res.rows[index];
-				console.log("*******************************************");
+		if(null == body){
+			return false;
+		}
+		client = new Elastical.Client("127.0.0.1", {port: 9200});
+		res = JSON.parse(body);
+		for(index in res.rows){
+			doc = res.rows[index];
+			console.log("........................................");
+			console.log(doc);
+			console.log("//////////////////////////////////////////");
+			console.log(index);
+			console.log("   .");
+			if(null == doc.id){
+				console.log("no doc id: ");
 				console.log(doc);
-				console.log(index);
-				if(null == doc.id){
-					console.log("no doc id: ");
-					console.log(doc);
-					continue;
-				}
-				doc.value.id = doc.id;
-				client.index("adminsearch", doc._type ? doc._type : "default", doc.value, function(err, result, res){
-					if(err){
-						console.log("err: ");
-						console.log(err);
-					}
-					if(result){
-						console.log("result: ");
-						console.log(result);
-					}
-					if(res){
-						console.log("res: ");
-						console.log(res);
-					}
-				});
+				continue;
 			}
+			doc.value.id = doc.id;
+			client.index("adminsearch", doc._type ? doc._type : "default", doc.value, function(err, result, res){
+				if(err){
+					console.log("error while indexing: ");
+					console.log(err);
+					console.log(doc);
+				}
+				if(result){
+					// console.log("result: ");
+					// console.log(result);
+				}
+				if(res){
+					// console.log("res: ");
+					// console.log(res);
+				}
+			});
 		}
 	}
 );
