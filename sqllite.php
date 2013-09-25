be58e2d6a215c88c2f067f30b4b7ab093474a779
viktorberzsinszky <vberzsin@gmail.com>
2013-09-25 12:48:26 +0200

    sqllite cozmeticka!

--- sqllite.php
+++ sqllite.php
@@ -10,29 +10,38 @@ $path_to_my_coutchbase_backup = "/Users/vico/Workspace/MyGassiBackend2/backend/v
 
 if(!($db = new SQLite3($path_to_my_coutchbase_backup))){
 	print "no such db\n";
-	return; 
+	exit(10);
 }
 
 print_r($db);
 
-if(!($q = $db->prepare("select * from cbb_msg"))){
+// if(!($q = $db->prepare('select * from cbb_msg'))){
+// if(!($q = $db->prepare('select val from cbb_msg'))){
+// if(!($q = $db->prepare('select * from cbb_msg where val like "%dog%" '))){
+if(!($q = $db->prepare('select cas, val from cbb_msg where val like "%dog%" '))){
 	print "corrupt statement\n";
-	exit(10);
+	exit(20);
 }
 
 print_r($q);
 
 if(!($res = $q->execute())){
 	print "no res for query\n";
-	exit(20);
+	print_r($q);
+	exit(30);
 }
 
 print_r($res);
 
+$i = 0;
 while($row = $res->fetchArray()){
-	print "***" . PHP_EOL;
+	print "////////////////////////////////////";
+	print PHP_EOL;
 	print_r($row);
 	print PHP_EOL;
+	print "\\\\\ " . $i;
+	print PHP_EOL;
+	$i++;
 }
 
 exit(30);
