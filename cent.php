858dfe697ab5d7fe62db0cc036361b69d487cdfe
viktorberzsinszky <vberzsin@gmail.com>
2013-09-26 11:59:47 +0200

    centkozmeticksc°

--- cent.php
+++ cent.php
@@ -4,14 +4,15 @@
 	to avoid the "EUR 10.00" + "EUR 10,12" f u n  
  						
 										*/
-class Fuck
+class PriceFormat
 { 
-	static $decim = ",";
-	public static function centsToCentsAOneHundredTimesRepr($cents)
+	static public $decim = ".";
+	static public function formatCentsToPrice($cents)
 	{
 		// hätte *fast* geklappt	
 		// return substr_replace($cents, self::$decim, strlen($cents) -2, 0);
 		$res = "";
+		// bruchcente gibt es ab jetzt nicht mehr
 		$cents = round($cents, 0);
 		$temp = str_split($cents);
 		$i = count($temp);
@@ -31,28 +32,23 @@ class Fuck
 	}
 }
 
-
-
-
-
-
 // ............ 
-function testCentsToCentsAOneHundredTimesRepr($cents)
+function testFormatCentsToPrice($cents)
 {
-	$res = Fuck::$decim = ",";
-	$res = Fuck::centsToCentsAOneHundredTimesRepr($cents);
+	$res = PriceFormat::$decim = ",";
+	$res = PriceFormat::formatCentsToPrice($cents);
 	print $res . PHP_EOL;
 }
 
-testCentsToCentsAOneHundredTimesRepr(40 +2000);
-testCentsToCentsAOneHundredTimesRepr(20 +12 +4 +200);
-testCentsToCentsAOneHundredTimesRepr(12 +3000 -2 -50000 -120);
-testCentsToCentsAOneHundredTimesRepr(12 +10);
-testCentsToCentsAOneHundredTimesRepr(12.1 +10.11);
-testCentsToCentsAOneHundredTimesRepr(12.1 +10.9999888811);
-testCentsToCentsAOneHundredTimesRepr(12.1 +10.49 -.1 -.1);
-testCentsToCentsAOneHundredTimesRepr(10000 +20000);
-testCentsToCentsAOneHundredTimesRepr(1 +2);
-testCentsToCentsAOneHundredTimesRepr(1 +2 +0);
-testCentsToCentsAOneHundredTimesRepr(11 +24);
+testFormatCentsToPrice(40 +2000);
+testFormatCentsToPrice(20 +12 +4 +200);
+testFormatCentsToPrice(12 +3000 -2 -50000 -120);
+testFormatCentsToPrice(12 +10);
+testFormatCentsToPrice(12.1 +10.11);
+testFormatCentsToPrice(12.1 +10.9999888811);
+testFormatCentsToPrice(12.1 +10.49 -.1 -.1);
+testFormatCentsToPrice(10000 +20000);
+testFormatCentsToPrice(1 +2);
+testFormatCentsToPrice(1 +2 +0);
+testFormatCentsToPrice(11 +24);
 
