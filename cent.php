db70fc889f652d5c050c623beac10356dadcded3
viktorberzsinszky <vberzsin@gmail.com>
2013-09-26 11:52:01 +0200

    y.:::::::::::::: :..:::

--- cent.php
+++ cent.php
@@ -2,20 +2,17 @@
 /*
     a wild wild wild attempt to store price values in their "cent" representation
 	to avoid the "EUR 10.00" + "EUR 10,12" f u n  
-
-
- 						*/
+ 						
+										*/
 class Fuck
 { 
 	static $decim = ",";
-	
 	public static function centsToCentsAOneHundredTimesRepr($cents)
 	{
 		// hätte *fast* geklappt	
 		// return substr_replace($cents, self::$decim, strlen($cents) -2, 0);
-		
-		$cents = round($cents, 0);
 		$res = "";
+		$cents = round($cents, 0);
 		$temp = str_split($cents);
 		$i = count($temp);
 		switch($i){
@@ -25,9 +22,9 @@ class Fuck
 			case 3: array_unshift($temp, 0); break;
 		}
 		$i = count($temp);
-		$dp = $i -3; 
+		$p = $i -3; 
 		while($i--){
-			if($dp == $i){ $res = self::$decim . $res; }
+			if($p == $i){ $res = self::$decim . $res; }
 			$res = $temp[$i] . $res;
 		}
 		return $res;
@@ -40,27 +37,22 @@ class Fuck
 
 
 // ............ 
-
-
-
-function testCentsToPrice($cents)
+function testCentsToCentsAOneHundredTimesRepr($cents)
 {
 	$res = Fuck::$decim = ",";
 	$res = Fuck::centsToCentsAOneHundredTimesRepr($cents);
 	print $res . PHP_EOL;
 }
 
-testCentsToPrice(40 +2000);
-testCentsToPrice(20 +12);
-testCentsToPrice(12 +3000);
-testCentsToPrice(12 +10);
-testCentsToPrice(12.1 +10.11);
-testCentsToPrice(12.1 +10.9999888811);
-testCentsToPrice(12.1 +10.49);
-testCentsToPrice(10000 +20000);
-testCentsToPrice(1 +2);
-testCentsToPrice(1 +2 +0);
-testCentsToPrice(11 +24);
-
-
+testCentsToCentsAOneHundredTimesRepr(40 +2000);
+testCentsToCentsAOneHundredTimesRepr(20 +12 +4 +200);
+testCentsToCentsAOneHundredTimesRepr(12 +3000 -2 -50000 -120);
+testCentsToCentsAOneHundredTimesRepr(12 +10);
+testCentsToCentsAOneHundredTimesRepr(12.1 +10.11);
+testCentsToCentsAOneHundredTimesRepr(12.1 +10.9999888811);
+testCentsToCentsAOneHundredTimesRepr(12.1 +10.49 -.1 -.1);
+testCentsToCentsAOneHundredTimesRepr(10000 +20000);
+testCentsToCentsAOneHundredTimesRepr(1 +2);
+testCentsToCentsAOneHundredTimesRepr(1 +2 +0);
+testCentsToCentsAOneHundredTimesRepr(11 +24);
 
